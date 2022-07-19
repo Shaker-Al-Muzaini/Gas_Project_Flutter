ea28805842a31e2d8af7411a85e317aeaefdbf93
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-07-19 21:27:05 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   runApp(const MyApp());    
 } 
 
-class MyApp extends StatelessWidget {   
+class MyApp extends StatelessWidget {    
   const MyApp({Key? key}) : super(key: key); 
 
   // This widget is the root of your application. 
